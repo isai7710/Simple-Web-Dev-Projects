ea176c628fea7941c8e4a036d11c2850cb584794
isai7710 <isai10sanchez@gmail.com>
2023-11-22 10:01:03 -0700

    adding simple unit converter app

---
+++ unit_converter/script.js
@@ -0,0 +1,35 @@
+const unitTypeSelected = document.getElementById('unit-type');
+const unitOptionsToSelectTop = document.getElementById('unit-input-options');
+const unitOptionsToSelectBottom = document.getElementById('unit-output-options');
+
+const unitsByType = {
+    length: ['cm', 'm', 'km'],
+    speed: ['m/s', 'km/h'],
+    temperature: ['°C', '°F'],
+    pressure: ['Pa', 'atm', 'bar']
+};
+
+// populate drop down information (units) for the appropriate select element
+function populateDropdown(selectElement, units) {
+    // create option element for each unit in units array ([cm, m, km])
+    // return an array of option elements into the const 'options' variable
+    const options = units.map(unit => {
+        const option = document.createElement('option');
+        option.value = unit;
+        option.textContent = unit;
+        return option;
+    });
+    selectElement.innerHTML = '';
+    options.forEach(option => selectElement.appendChild(option));
+}
+
+function populateUnits() {
+    const units = unitsByType[unitTypeSelected.value];
+    populateDropdown(unitOptionsToSelectTop, units);
+    populateDropdown(unitOptionsToSelectBottom, units);
+}
+
+
+unitTypeSelected.addEventListener('change', populateUnits);
+
+populateUnits(); // Populate initially
