5823e1b559294e78fda2614b540af37a3e88f197
isai7710 <isai10sanchez@gmail.com>
2023-12-13 02:34:50 -0700

    optimized converLength function by using a nested object variable (conversionFactors)

--- unit_converter/script.js
+++ unit_converter/script.js
@@ -2,20 +2,29 @@ const unitTypeSelected = document.getElementById('unit-type');
 const unitOptionsToSelectTop = document.getElementById('unit-input-options');
 const unitOptionsToSelectBottom = document.getElementById('unit-output-options');
 
-const conversion_factors_length = {
-    mm_to_cm: 10,
-    mm_to_m: 1000,
-    mm_to_km: 1000*1000,
-    cm_to_m: 100,
-    cm_to_km: 100*1000,
-    m_to_km: 1000
-}
-
 const unitsByType = {
     length: ['mm', 'cm', 'm', 'km', 'in', 'ft', 'mi'],
     speed: ['m/s', 'km/h', 'mph', 'kph'],
     temperature: ['°C', '°F', 'K'],
     pressure: ['Pa', 'atm', 'bar']
+};
+
+const conversionFactors = {
+    // TODO: Conversion factors for imperial units (in, ft, mi) and other units (temperature, pressure, etc.)
+    length: {
+        mm_to_cm: 0.1,      // 1 mm = 0.1 cm
+        mm_to_m: 0.001,     // 1 mm = 0.001 m
+        mm_to_km: 0.000001, // 1 mm = 0.000001 km (or 0.001 * 0.001 for clarification)
+        cm_to_mm: 10,       // 1 cm = 10 mm
+        cm_to_m: 0.01,      // 1 cm = 0.01 m
+        cm_to_km: 0.00001,  // 1 cm = 0.00001 km (or 0.01 * 0.001 for clarification)
+        m_to_mm: 1000,      // 1 m = 1000 mm
+        m_to_cm: 100,       // 1 m = 100 cm
+        m_to_km: 0.001,     // 1 m = 0.001 km
+        km_to_mm: 1000000,  // 1 km = 1000000 mm (or 1000 * 1000 for clarification)
+        km_to_cm: 100000,   // 1 km = 100000 cm (or 100 * 1000 for clarification)
+        km_to_m: 1000       // 1 km = 1000 m
+    }
 };
 
 // populate drop down information (units) for the appropriate select element
@@ -43,8 +52,8 @@ function convertUnits(){
     const inputValue = parseFloat(document.getElementById('input-amount').value);;
     let outputValue = 0;
 
-    if (inputValue == null){
-        appendErrorParagraph('no amount inputed, please input amount and try again');
+    if (inputValue == null || isNaN(inputValue)){
+        appendErrorParagraph('no valid amount inputted, please input amount and try again');
     }
 
     switch (unitSelected){
@@ -57,9 +66,11 @@ function convertUnits(){
 
 }
 
-function convertLength(v, inputUnits, outputUnits){
-    if (inputUnits == 'mm' && outputUnits == 'cm'){
-        return v * conversion_factors_length.mm_to_cm;
+function convertLength(value, inputUnits, outputUnits){
+    if (conversionFactors.length[inputUnits + '_to_' + outputUnits]) {
+        return value * conversionFactors.length[inputUnits + '_to_' + outputUnits];
+    } else if (inputUnits == outputUnits) {
+        return value;
     }
 }
 
